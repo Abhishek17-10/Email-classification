35a28bece2496e611bc4e5d26a6ed5dc077ee401
Abhishek Singh <abhi007sagar@gmail.com>
2025-04-22 23:33:47 +0530

    Update app_gradio.py

--- app_gradio.py
+++ app_gradio.py
@@ -4,17 +4,15 @@ from transformers import DistilBertTokenizer, DistilBertForSequenceClassificatio
 from pii_masker import mask_pii
 from utils import label_mapping
 
-# Model path (should match your folder name)
+
 model_path = "email_classifier_model"
 
-# Load tokenizer and model
 tokenizer = DistilBertTokenizer.from_pretrained(model_path)
 model = DistilBertForSequenceClassification.from_pretrained(model_path)
 model.eval()
 
-# Inference function
-def classify_email(text):
-    masked_text, _ = mask_pii(text)  # second value is entities list, not needed here
+
+    masked_text, _ = mask_pii(text)  
     inputs = tokenizer(masked_text, return_tensors="pt", truncation=True, padding=True)
     with torch.no_grad():
         outputs = model(**inputs)
@@ -23,7 +21,7 @@ def classify_email(text):
     label = label_mapping.get(pred, "Unknown")
     return f"Prediction: {label}\n\nMasked Email:\n{masked_text}"
 
-# Gradio interface
+
 iface = gr.Interface(
     fn=classify_email,
     inputs=gr.Textbox(lines=12, label="Paste your email below:"),
